f0806b1aa3c1f008f4b4f9e461e8c4a29a446f8d
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-09-28 23:55:46 -0300

    Refazendo projeto com outra ideia

--- src/br/com/umacem/UmACem.java
+++ src/br/com/umacem/UmACem.java
@@ -11,56 +11,10 @@ import javax.swing.JOptionPane;
  */
 public class UmACem {
 	
-	//Main
-	public static void main(String[] args) {
-		//Inicia jogo
-		if (!iniciaJogo()) {
-			JOptionPane.showInputDialog("Obrigado e volte Sempre!");
-			System.exit(0);
-		};
+	public static void main(String args[]) {
 		
-		iniciaPalpites();
-	}
-	
-	//Métodos
-	
-	public static int iniciaPalpites() {
 		
-		String palpite = JOptionPane.showInputDialog("O número que você pensou é maior ou menor que 50?");
-		
-		if(palpite.toLowerCase().equals("sim")) {
-			maiorQue50();
-		} else {
-			menorQue50();
-		}
-		return 0;
-	}
-
-	private static void menorQue50() {
-		// TODO Auto-generated method stub
 		
 	}
-
-	private static void maiorQue50() {
-		// TODO Auto-geneated method stub
-		
-	}
-
-	public static boolean iniciaJogo() {
-		
-		String confirmacao = JOptionPane.showInputDialog("Pense em um número de 1 a 100. \n"
-				+ "Insira Sim para continuar ou Sair para sair.");
-		
-		confirmacao = confirmacao.toLowerCase();
-		
-		if(confirmacao.equals("sim")) {
-			return true;
-		} else if(!confirmacao.equals("sair")) {
-			iniciaJogo();
-		}
-		return false;
-	}
-	
-	
 
 }
